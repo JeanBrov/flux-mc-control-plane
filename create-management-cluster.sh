97d635b45ca85d9e0ffcde47c1106f153812ae5e
Ivan.Kryskun <ivan.kriskun@gmail.com>
2023-08-24 17:15:46 +0300

    Updated git

--- create-management-cluster.sh
+++ create-management-cluster.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
-kind create cluster --name management
-flux install
+kind create cluster --name management.9
+840
+120.3.141flux install
 kubectl apply -f control-plane/gitrepo.yaml
 kubectl apply -f control-plane/ks.yaml
